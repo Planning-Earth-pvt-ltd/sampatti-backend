c7ea3854cccfb76911d87f46908f9d758c42938c
Aggarwal-Vishal <007vishal.aggarwal@gmail.com>
2025-07-02 16:04:16 +0530

    Hosting changes Fixed

--- src/index.ts
+++ src/index.ts
@@ -25,7 +25,7 @@ app.use('/api/v1/property', propertyRoutes);
 // Server and database starting
 async function startServer(): Promise<void>  {
     try {
-      const PORT = process.env.PORT;
+      const PORT = process.env.PORT || 5000;
 
    await prisma.$connect();
    console.log("Database Connected");
